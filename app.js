47c6f28086d87b1565ac128304adcfd9f82d8bde
GemignaniAlejandro <alejandrogemignani1@gmail.com>
2021-03-02 20:22:07 -0300

    Desafío 5 terminado.

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const http = require('http');
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((request, response)=>{
-    response.end("Hola");
-});
+const getRandomNum = (min, max) => 
+{
+    return Math.random() * (max-min) + min;
+}
 
-server.listen(8080, function(){
-    console.log("El servidor está activo en el puerto: "+ this.address().port);
+const requestListener = (req, res) => 
+{
+    res.writeHead(200);
+    const objetoRes = 
+    {
+        id: Math.round(getRandomNum(1,10)),
+        title: "Producto: " + Math.round(getRandomNum(1,10)),
+        price: getRandomNum(0.00,9999.99).toFixed(2),
+        thumbnail: "Foto: " + Math.round(getRandomNum(1,10)) 
+    }
+
+    res.end(JSON.stringify(objetoRes));
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(PORT, function(){
+    console.log(`El servidor está en el puerto: ${PORT}`);
 })
